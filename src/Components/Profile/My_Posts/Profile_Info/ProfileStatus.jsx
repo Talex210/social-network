120a4efa8a92d2f04482abc0b1152705e0a55705
Talex210 <tal.89@mail.ru>
2023-01-23 20:18:04 +0300

    73 componentDidUpdate

--- src/Components/Profile/My_Posts/Profile_Info/ProfileStatus.jsx
+++ src/Components/Profile/My_Posts/Profile_Info/ProfileStatus.jsx
@@ -19,6 +19,14 @@ class ProfileStatus extends React.Component {
         this.setState({status: event.target.value})
     }
 
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status,
+            })
+        }
+    }
+
     render() {
         return (
             <div>
